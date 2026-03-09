a359c3d1217062399ec8c05407de1d8b0f18df99
Harvey Burman <harveyoliverburman@outlook.com>
2026-03-09 14:23:27 +0000

    changed base for hosting

--- memory-match/vite.config.js
+++ memory-match/vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/memory-match/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
